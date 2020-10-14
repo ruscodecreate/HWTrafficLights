7478d8b7a6e700640955ef4e080efc8896621868
ruscodecreate <cpabuild@gmail.com>
2020-10-14 17:16:17 +0100

    Working with code

--- HWTrafficLights/ViewController.swift
+++ HWTrafficLights/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        startButton.layer.cornerRadius = 10
+        redLightView.alpha = lightIsOn
+        yellowLightView.alpha = lightIsOff
+        greenLightView.alpha = lightIsOff
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        redLightView.layer.cornerRadius = redLightView.frame.width / 2
+        yellowLightView.layer.cornerRadius = redLightView.frame.width / 2
+        greenLightView.layer.cornerRadius = redLightView.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        
+        startButton.titleLabel?.text = "NEXT"
+        
+        switch currentLight {
+        
+        case .red:
+            currentLight = .yellow
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOn
+        case .yellow:
+            currentLight = .green
+            redLightView.alpha = lightIsOff
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOn
+        case .green:
+            currentLight = .red
+            redLightView.alpha = lightIsOn
+            yellowLightView.alpha = lightIsOff
+            greenLightView.alpha = lightIsOff
+        }
+    }
 }
+
 
